ac636c3b829b5201647b6d7af78b61e083d240a2
anya <anya>
2023-10-11 09:54:55 +0700

    add push registry service account

--- main.tf
+++ main.tf
@@ -10,3 +10,18 @@ resource "google_artifact_registry_repository" "registry" {
   }
 }
 
+# Push Image to Artifact Registry Service Account
+resource "google_service_account" "registry-sa" {
+  account_id = "push-registry"
+}
+
+resource "google_service_account_key" "registry-sa-key" {
+  service_account_id = google_service_account.registry-sa.name
+  public_key_type    = "TYPE_X509_PEM_FILE"
+}
+
+resource "google_project_iam_member" "registry-sa-member" {
+  project = var.project
+  role    = "roles/artifactregistry.admin"
+  member  = "serviceAccount:${google_service_account.registry-sa.email}"
+}
